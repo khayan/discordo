5489af33ca393cf8e585189c88c6953e34914cb3
Ubuntu <ubuntu@ip-172-31-81-243.ec2.internal>
2019-11-26 23:18:22 +0000

    Primeiros arquivos

---
+++ src/Handler/Home.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+module Handler.Home where
+
+import Import
+import Network.HTTP.Types.Status
+import Database.Persist.Postgresql
+
+getHomeR :: Handler Html
+getHomeR = do 
+    defaultLayout $ do 
+        toWidgetHead [julius|
+            function ola(){
+                alert("ola");
+            }
+        |]
+        toWidgetHead [lucius|
+            h1 {
+                color : red;
+            }
+        |]
+        [whamlet|
+            <h1>
+                OLA MUNDO!
+
+            <button onclick="ola()">
+                OLA
+        |]
