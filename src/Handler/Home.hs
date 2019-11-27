fe1fbbd6bc766bc755c373fef9c6c9c104d8d249
Ubuntu <ubuntu@ip-172-31-81-243.ec2.internal>
2019-11-27 14:04:30 +0000

    comentario

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -12,11 +12,11 @@ import Database.Persist.Postgresql
 getHomeR :: Handler Html
 getHomeR = do 
     defaultLayout $ do 
-        toWidgetHead [julius|
-            function ola(){
-                alert("ola")
-            }
-        |]
+    --    toWidgetHead [julius|
+    --        function ola(){
+    --            alert("ola");
+    --        }
+    --    |]
         toWidgetHead [lucius|
             h1 {
                 color : red;
