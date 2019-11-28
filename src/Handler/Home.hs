1531cbcc39a69b549f700da8568f94f15ab0eceb
Ubuntu <ubuntu@ip-172-31-81-243.ec2.internal>
2019-11-28 18:28:14 +0000

    teste

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -2,12 +2,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TypeFamilies #-}
 module Handler.Home where
 
 import Import
-import Text.Julius
+import Data.FileEmbed(embedFile)
 import Text.Lucius
+import Text.Julius
+--import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
 
 getHomeR :: Handler Html
