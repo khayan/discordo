a464c6aef2e2ec6a4e1ce0c693f09f8c434cba8d
Ubuntu <ubuntu@ip-172-31-81-243.ec2.internal>
2019-11-28 00:02:16 +0000

    Teste home

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -6,13 +6,23 @@
 module Handler.Home where
 
 import Import
-import Text.Julius
-import Text.Lucius
+--import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
 
 getHomeR :: Handler Html
-getHomeR = do 
-    defaultLayout $ do 
-        toWidgetHead $(luciusFile "templates/default-layout.lucius")
-        $(whamletFile "templates/aaa.hamlet")
-        
+getHomeR = do
+    defaultLayout $ do
+        toWidgetHead [julius|
+            function ola(){
+                alert("ola mundo";)
+            }
+        |]
+        toWidgetHead [lucius|
+            h1{
+                color: red;
+            }
+        |]
+        [whamlet|
+            <h1>Olá mundo
+            <button onclick="ola()">Clickar
+        |]
