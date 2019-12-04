39debdc0dcc0e7ed6ec43b21d523b2dbb5019e75
khayan <ubuntu@ip-172-31-81-243.ec2.internal>
2019-12-04 14:18:52 +0000

    Formulário de login #12

--- src/Handler/Login.hs
+++ src/Handler/Login.hs
@@ -43,7 +43,6 @@ getHomeLoginR = do
                         <input type="submit" name="action" value="Login">
                         <p>Não está cadastrado? <a href="contato">Crie uma conta</a></p>
 
-        
                   <form action=@{AuthenticationR} method=post enctype=#{enctype}>
                     ^{widget}
                     <button class="btn waves-effect waves-light" type="submit" name="action">Logar
