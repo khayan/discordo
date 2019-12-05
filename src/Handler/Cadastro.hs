da09be212ebcfa4ebf924e1c3ced1fd22b29a975
khayan <ubuntu@ip-172-31-81-243.ec2.internal>
2019-12-05 01:33:40 +0000

    Cadastro  #04

--- src/Handler/Cadastro.hs
+++ src/Handler/Cadastro.hs
@@ -4,7 +4,7 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TypeFamilies #-}
-module Handler.Landing where
+module Handler.Cadastro where
 
 import Import
 import Data.FileEmbed(embedFile)
