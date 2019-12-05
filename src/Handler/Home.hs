4c06c40eda4671c7d5f410e4c3d677227ed24743
khayan <ubuntu@ip-172-31-81-243.ec2.internal>
2019-12-05 17:44:07 +0000

    Home #08

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -37,8 +37,8 @@ getHomeR = do
         [whamlet|
             <nav>
                 <ul class="menu">
-                    <li class="logo"><a href="@{HomeR}">Discordo!
-                    <li class="item"><a href="@{ExitLogoutR}">Sair
+                    <li class="logo"><a href="@{HomeR}">Discordo!</a>
+                    <li class="item"><a href="@{ExitLogoutR}">Sair</a>
                     <li class="toggle"><span class="bars">
         
             <header>
