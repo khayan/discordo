6afefa8dafb7d400b535cb39ff6fd6969ab4b334
khayan <ubuntu@ip-172-31-81-243.ec2.internal>
2019-12-05 21:23:41 +0000

    Home #26

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -58,8 +58,8 @@ getHomeR = do
                                 <button class="agree" onClick(contador("concordo"))>Concordo!
                                 <button class="disagree" onClick(contador("discordo"))>Discordo!
                             <div class="contador">
-                                <p>&#128077; <script>getConcordo()
-                                <p>&#128078; <script>getDiscordo()
+                                <p>&#128077; <script>getConcordo()</script>
+                                <p>&#128078; <script>getDiscordo()</script>
                             
             <footer>
                 <p>
